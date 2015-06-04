ac831612a4efc252b139ba11fd4f98acbb999bba
mbrentini <marco.brentini@dpd.fr>
2015-06-04 18:28:47 +0200

    Version 5.1.2
    - Shipping rates configuration is now in JSON format, updated Owebia
    core file
    - Updated default configuration
    - Updated headers of files
    - Updated documentation

--- skin/frontend/base/default/css/dpdfrance/dpdfrpredict.css
+++ skin/frontend/base/default/css/dpdfrance/dpdfrpredict.css
@@ -1,10 +1,10 @@
 /**
- * DPD France v5.1.0 shipping module for Magento
+ * DPD France v5.1.2 shipping module for Magento
  *
  * @category   DPDFrance
  * @package    DPDFrance_Shipping
- * @author     DPD S.A.S. <ensavoirplus.ecommerce@dpd.fr>
- * @copyright  2015 DPD S.A.S., société par actions simplifiée, au capital de 18.500.000 euros, dont le siège social est situé 27 Rue du Colonel Pierre Avia - 75015 PARIS, immatriculée au registre du commerce et des sociétés de Paris sous le numéro 444 420 830 
+ * @author     DPD France S.A.S. <ensavoirplus.ecommerce@dpd.fr>
+ * @copyright  2015 DPD France S.A.S., société par actions simplifiée, au capital de 18.500.000 euros, dont le siège social est situé 27 Rue du Colonel Pierre Avia - 75015 PARIS, immatriculée au registre du commerce et des sociétés de Paris sous le numéro 444 420 830 
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
